2b722bba16cfd017ac3db75832e4f8bf7b77a991
Pontus A <pontus.abrahamsson@iths.se>
2023-12-06 14:10:39 +0100

    Add cors to sign in route

--- backend/routes/auth.routes.ts
+++ backend/routes/auth.routes.ts
@@ -1,15 +1,16 @@
 import verifySignUp from '../middleware/verifySignUp';
 import { signin, signup } from '../controllers/auth.controller';
 import { Express } from 'express-serve-static-core';
+import cors from 'cors';
 
 export const authRoutes = (app: Express) => {
-  app.use(function (req, res, next) {
+  app.use((req, res, next) => {
     res.header(
       'Access-Control-Allow-Headers',
       'x-access-token, Origin, Content-Type, Accept'
     );
     next();
-  });
+  }, cors());
 
   app.post(
     '/api/auth/signup',
