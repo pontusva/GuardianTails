5c0104abf8279b14c5a476937e933db998818898
Pontus A <pontus.abrahamsson@iths.se>
2023-12-06 14:06:29 +0100

    Add login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,14 +1,128 @@
-export default function LoginPage() {
+import { useForm, SubmitHandler } from 'react-hook-form';
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+
+interface IFormInput {
+  username: string;
+  password: string;
+}
+
+export default function Login() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<IFormInput>({
+    mode: 'onChange',
+  });
+
+  const onSubmit: SubmitHandler<IFormInput> = async (values: IFormInput) => {
+    console.log(values);
+
+    const respons = await fetch('http://localhost:8080/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: values.username,
+        password: values.password,
+      }),
+    });
+    const result = await respons.json();
+
+    console.log(result);
+    console.log(errors);
+  };
+
   return (
-    <div className="LoginPage">
-      <h1>Login</h1>
-      <form>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-        <button type="submit">Login</button>
-      </form>
+    <div
+      style={{ height: '100dvh', width: '100dvw' }}
+      className="flex flex-col justify-center items-center"
+      // className="bg-contain  bg-no-repeat bg-bottom object-contain bg-[url('/basicdragonfly.svg')]"
+    >
+      <div className="mt-20 w-80">
+        <h1 className=" text-center mb-1 font-mono text-3xl">GuradianTails</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack spacing={5}>
+            <FormControl isInvalid={!!errors.username}>
+              <div>
+                <FormLabel margin={0} htmlFor="email">
+                  Enter your username
+                </FormLabel>
+                <Input
+                  borderColor="#3b444b"
+                  opacity={0.9}
+                  borderWidth={2}
+                  id="username"
+                  type="text"
+                  placeholder="username"
+                  {...register('username', {
+                    required: 'This is required',
+                    minLength: {
+                      value: 2,
+                      message: 'Minimum length should be 4',
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </div>
+            </FormControl>
+            <div>
+              <FormControl isInvalid={!!errors.password}>
+                <div>
+                  <FormLabel margin={0} className="" htmlFor="password">
+                    password
+                  </FormLabel>
+                  <Input
+                    borderColor="#3b444b"
+                    opacity={0.9}
+                    borderWidth={2}
+                    id="password"
+                    type="password"
+                    placeholder="Enter password"
+                    {...register('password', {
+                      required: 'This is required',
+                      minLength: {
+                        value: 2,
+                        message: 'Minimum length should be 4',
+                      },
+                    })}
+                  />
+                </div>
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </FormControl>
+            </div>
+          </Stack>
+
+          <div>
+            <p className="text-right my-2 cursor-pointer italic">
+              Dont have an account?
+            </p>
+            <Button
+              width={80}
+              colorScheme="teal"
+              isLoading={isSubmitting}
+              type="submit">
+              Login
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-5">
+          <img src="/basicdragonfly.svg" />
+        </div>
+      </div>
     </div>
   );
 }
