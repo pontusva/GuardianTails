a1281365569847ad878159216fe69c3b1c311c4c
Pontus A <pontus.abrahamsson@iths.se>
2023-12-17 19:16:06 +0100

    Refactor

--- backend/controllers/ai.controller.ts
+++ backend/controllers/ai.controller.ts
@@ -3,6 +3,9 @@ import { main, thread } from '../oai/init';
 import dbInitFunction from '../models';
 
 const db = dbInitFunction();
+if (!db) {
+  throw new Error('Database initialization failed');
+}
 const User = db?.user;
 
 export const oai = async (req: Request, res: Response) => {
@@ -18,7 +21,7 @@ export const createThread = async (req: Request, res: Response) => {
   const { user_id } = req.body;
   const thread_id = (await thread()).thread.id;
   const assistant_id = (await thread()).assistant.id;
-  await User!.update({ thread_id, assistant_id }, { where: { user_id } });
+  await User.update({ thread_id, assistant_id }, { where: { user_id } });
   res.send(thread_id);
 };
 
@@ -26,9 +29,7 @@ export const isUserThread = async (req: Request, res: Response) => {
   const { user_id } = req.body;
   const user = await User!.findOne({ where: { user_id: user_id } });
 
-  if (user!.thread_id) {
-    res.send({ thread: true });
-  } else {
-    res.send({ thread: false });
-  }
+  user && user.thread_id
+    ? res.send({ thread: true })
+    : res.send({ thread: false });
 };
