f636b30407963fbf9054ab987140916e182237d9
Pontus A <pontus.abrahamsson@iths.se>
2023-12-19 17:08:13 +0100

    Add useState hook and fetch pet images

--- frontend/src/pages/AuthPages/UserPages/SpecificPetPage.tsx
+++ frontend/src/pages/AuthPages/UserPages/SpecificPetPage.tsx
@@ -1,8 +1,11 @@
 import { useLocation } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+interface LostPet {}
 
 export default function SpecificPetPage() {
+  const [pet, setPet] = useState<LostPet[]>([]);
   const location = useLocation();
   const id = location.pathname.split('/').pop();
   const getPet = async () => {
@@ -17,13 +20,32 @@ export default function SpecificPetPage() {
       }
     );
     const data = await response.json();
-    console.log(data);
+
     return data;
   };
 
   useEffect(() => {
-    getPet();
+    getPet().then(async data => {
+      // const fetchPromises = data.map((pet: LostPet) =>
+      //   fetch(
+      //     `http://localhost:8080/images/${pet.ImageGalleries[0].image_url}`,
+      //     {
+      //       method: 'GET',
+      //       headers: {
+      //         'Content-Type': 'application/json',
+      //         Authorization: `Bearer ${Cookies.get('token')}`,
+      //       },
+      //     }
+      //   )
+      //     .then(response => response.blob())
+      //     .then(blob => ({ pet, image_url: URL.createObjectURL(blob) }))
+      // );
+      // const petImages = await Promise.all(fetchPromises);
+      // setPet(petImages);
+    });
   }, []);
+
+  console.log(pet);
   return (
     <div>
       <h1>SpecificPetPage</h1>
