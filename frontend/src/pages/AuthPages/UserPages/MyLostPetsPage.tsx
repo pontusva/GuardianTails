983498241df3f4643fd0dd29e0f09ab189bd3c7b
Pontus A <pontus.abrahamsson@iths.se>
2023-12-20 17:24:57 +0100

    Refactor MyLostPetsPage component

--- frontend/src/pages/AuthPages/UserPages/MyLostPetsPage.tsx
+++ frontend/src/pages/AuthPages/UserPages/MyLostPetsPage.tsx
@@ -21,8 +21,7 @@ interface ImageGallery {
 }
 
 export default function MyLostPetsPage() {
-  const [petInfo, setPetInfo] = useState<ImageGallery[]>([]);
-
+  const [result, setResult] = useState<LostPet[]>([]);
   const navigation = useNavigate();
 
   const getUsersLostPets = async () => {
@@ -40,51 +39,32 @@ export default function MyLostPetsPage() {
       }
     );
     const data = await response.json();
-
+    setResult(data);
     return data;
   };
 
   useEffect(() => {
-    getUsersLostPets().then(async data => {
-      const fetchPromises = data.map((pet: LostPet) =>
-        fetch(
-          `http://localhost:8080/images/${pet.ImageGalleries[0].image_url}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${Cookies.get('token')}`,
-            },
-          }
-        )
-          .then(response => response.blob())
-          .then(blob => ({ pet, image_url: URL.createObjectURL(blob) }))
-      );
-      console.log(data);
-      const petImages = await Promise.all(fetchPromises);
-
-      setPetInfo(petImages);
-    });
+    getUsersLostPets();
   }, []);
-
-  useEffect(() => {
-    return () => {
-      petInfo.forEach(petImage => URL.revokeObjectURL(petImage.image_url));
-    };
-  }, [petInfo]);
 
   return (
     <>
-      {petInfo.map((petImage, index) => (
-        <div className="flex flex-col items-center" key={index}>
-          <h1
-            onClick={() => navigation(`/pet/${petImage.pet.pet_id}`)}
-            className="mt-8 text-2xl">
-            {petImage.pet.name}
-          </h1>
-          <img className="object-fit w-96" src={petImage.image_url} alt="Pet" />
-        </div>
-      ))}
+      {result.map((petImage, index) => {
+        console.log(result);
+        return (
+          <div className="flex flex-col items-center" key={index}>
+            <h1
+              onClick={() => navigation(`/pet/${petImage.pet_id}`)}
+              className="mt-8 text-2xl">
+              {petImage.name}
+            </h1>
+            <img
+              src={`http://localhost:8080/images/${petImage.ImageGalleries[0].image_url}`}
+              alt=""
+            />
+          </div>
+        );
+      })}
     </>
   );
 }
