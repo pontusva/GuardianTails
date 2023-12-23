99d08106fc49cb1222630d17fef9ad7839d024c2
Pontus A <pontus.abrahamsson@iths.se>
2023-12-23 09:38:42 +0100

    Add getAllPetAlerts function to fetch all lost pets

--- frontend/src/pages/AuthPages/UserPages/PetAlerts.tsx
+++ frontend/src/pages/AuthPages/UserPages/PetAlerts.tsx
@@ -1,4 +1,22 @@
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
+
 export default function PetAlerts() {
+  const getAllPetAlerts = async () => {
+    const response = await fetch('http://localhost:5000//api/all-lost-pets', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Cookies.get('token')}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getAllPetAlerts();
+  }, []);
   return (
     <div>
       <p>PetAlerts</p>
